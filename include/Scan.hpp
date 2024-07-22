6351ae9a84eb5907d2e2d9f18c90b4c9a516d3d6
Alessandro Catenacci <alessandrocatenacci22@gmail.com>
2024-07-22 12:55:23 +0200

    Indentation

--- include/Scan.hpp
+++ include/Scan.hpp
@@ -1,14 +1,16 @@
-#include "WiFiScanData.hpp"
-#include "../include/wifi_scan.h"
-#include <vector>
 #include <unordered_set>
+#include <vector>
+
+#include "../include/wifi_scan.h"
+#include "WiFiScanData.hpp"
 
 class Scan {
 public:
-    Scan(const std::string& interface);
-    bool scan(std::vector<std::vector<WiFiScanData>>& wifiScanDataVec, bool filter);
+  Scan(const std::string &interface);
+  bool scan(std::vector<std::vector<WiFiScanData>> &wifiScanDataVec,
+            bool filter);
 
 private:
-    std::string interface;
-    std::unordered_set<std::string> targetBSSIDs;
+  std::string interface;
+  std::unordered_set<std::string> targetBSSIDs;
 };
--- src/Scan.cpp
+++ src/Scan.cpp
@@ -1,119 +1,126 @@
-#include "../include/Scan.hpp"
-#include <stdexcept>
-#include <cstdio>
-#include <iostream>
 #include <Eigen/Dense>
-#include <cmath>
 #include <algorithm>
-#include <iterator>
+#include <cmath>
+#include <cstdio>
 #include <fstream>
-#include <sstream>
-#include <numeric>
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
+#include <iostream>
+#include <iterator>
 #include <map>
+#include <numeric>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "../include/Scan.hpp"
 #include "../include/wifi_scan.h"
 
-Scan::Scan(const std::string& interface)
-    : interface(interface) {
-    if (interface.empty()) {
-        throw std::invalid_argument("Interface name cannot be empty");
-    }
+Scan::Scan(const std::string &interface) : interface(interface) {
+  if (interface.empty()) {
+    throw std::invalid_argument("Interface name cannot be empty");
+  }
 }
 
-bool Scan::scan(std::vector<std::vector<WiFiScanData>>& wifiScanDataVec, bool filter) {
-    const int maxAPs = 100;
-    const int numScans = 10;
-    std::unordered_map<std::string, std::vector<WiFiScanData>> wifiDataMap;
-    std::unordered_map<std::string, double> frequencyMap;
+bool Scan::scan(std::vector<std::vector<WiFiScanData>> &wifiScanDataVec,
+                bool filter) {
+  const int maxAPs = 100;
+  const int numScans = 10;
+  std::unordered_map<std::string, std::vector<WiFiScanData>> wifiDataMap;
+  std::unordered_map<std::string, double> frequencyMap;
 
-    struct bss_info bssInfos[maxAPs]; 
+  struct bss_info bssInfos[maxAPs];
 
-    for (int scanIdx = 0; scanIdx < numScans; ++scanIdx) {
-        wifi_scan* wifi = wifi_scan_init(interface.c_str());
-        if (!wifi) {
-            std::cerr << "Failed to initialize WiFi scan on interface " << interface << std::endl;
-            return false;
-        }
-
-        int numAPs = wifi_scan_all(wifi, bssInfos, maxAPs);
-
-        if (numAPs < 0) {
-            std::cerr << "Failed to perform WiFi scan on interface " << interface << std::endl;
-            return false;
-        }
-
-        for (int i = 0; i < numAPs && i < maxAPs; ++i) {
-            char bssidStr[BSSID_STRING_LENGTH];
-            snprintf(bssidStr, BSSID_STRING_LENGTH, "%02x:%02x:%02x:%02x:%02x:%02x",
-                     bssInfos[i].bssid[0], bssInfos[i].bssid[1], bssInfos[i].bssid[2],
-                     bssInfos[i].bssid[3], bssInfos[i].bssid[4], bssInfos[i].bssid[5]);
-
-            std::string mac(bssidStr);
-
-            if (!filter || targetBSSIDs.find(mac) != targetBSSIDs.end()) {
-                double rssi = 30 - (bssInfos[i].signal_mbm / 100) ;
-                frequencyMap[mac] = bssInfos[i].frequency;
-
-                WiFiScanData data(mac, rssi, 0.0, 0.0, bssInfos[i].frequency);
-                wifiDataMap[mac].push_back(data);
-            }
-        }
-
+  for (int scanIdx = 0; scanIdx < numScans; ++scanIdx) {
+    wifi_scan *wifi = wifi_scan_init(interface.c_str());
+    if (!wifi) {
+      std::cerr << "Failed to initialize WiFi scan on interface "
+                << interface << std::endl;
+      return false;
     }
 
-    wifiScanDataVec.clear();
+    int numAPs = wifi_scan_all(wifi, bssInfos, maxAPs);
 
-    if (wifiDataMap.empty()) {
-        std::cerr << "No APs found after scanning." << std::endl;
-        return false;
+    if (numAPs < 0) {
+      std::cerr << "Failed to perform WiFi scan on interface "
+                << interface << std::endl;
+      return false;
     }
 
-    for (auto& entry : wifiDataMap) {
-        const std::string& mac = entry.first;
-        std::vector<WiFiScanData>& wifiData = entry.second;
+    for (int i = 0; i < numAPs && i < maxAPs; ++i) {
+      char bssidStr[BSSID_STRING_LENGTH];
+      snprintf(bssidStr, BSSID_STRING_LENGTH, "%02x:%02x:%02x:%02x:%02x:%02x",
+               bssInfos[i].bssid[0], bssInfos[i].bssid[1], bssInfos[i].bssid[2],
+               bssInfos[i].bssid[3], bssInfos[i].bssid[4],
+               bssInfos[i].bssid[5]);
 
-        double mean = std::accumulate(wifiData.begin(), wifiData.end(), 0.0,
-                                      [](double sum, const WiFiScanData& data) {
-                                          return sum + data.rssi;
-                                      }) / wifiData.size();
+      std::string mac(bssidStr);
 
-        double sq_sum = std::accumulate(wifiData.begin(), wifiData.end(), 0.0,
-                                        [mean](double sum, const WiFiScanData& data) {
-                                            return sum + (data.rssi - mean) * (data.rssi - mean);
-                                        });
-        double stddev = std::sqrt(sq_sum / wifiData.size());
+      if (!filter || targetBSSIDs.find(mac) != targetBSSIDs.end()) {
+        double rssi = 30 - (bssInfos[i].signal_mbm / 100);
+        frequencyMap[mac] = bssInfos[i].frequency;
 
-        for (auto& data : wifiData) {
-            data.mean = mean;
-            data.std = stddev;
-        }
+        WiFiScanData data(mac, rssi, 0.0, 0.0, bssInfos[i].frequency);
+        wifiDataMap[mac].push_back(data);
+      }
+    }
+  }
 
-        wifiScanDataVec.push_back(wifiData);
+  wifiScanDataVec.clear();
+
+  if (wifiDataMap.empty()) {
+    std::cerr << "No APs found after scanning." << std::endl;
+    return false;
+  }
+
+  for (auto &entry : wifiDataMap) {
+    const std::string &mac = entry.first;
+    std::vector<WiFiScanData> &wifiData = entry.second;
+
+    double mean = std::accumulate(wifiData.begin(), wifiData.end(), 0.0,
+                                  [](double sum, const WiFiScanData &data) {
+                                    return sum + data.rssi;
+                                  }) /
+                  wifiData.size();
+
+    double sq_sum =
+        std::accumulate(wifiData.begin(), wifiData.end(), 0.0,
+                        [mean](double sum, const WiFiScanData &data) {
+                          return sum + (data.rssi - mean) * (data.rssi - mean);
+                        });
+    double stddev = std::sqrt(sq_sum / wifiData.size());
+
+    for (auto &data : wifiData) {
+      data.mean = mean;
+      data.std = stddev;
     }
 
-    std::ofstream outputFile("scan_data.csv");
-    outputFile << "MAC Address,RSSI Value,Mean,STD,Frequency\n";
+    wifiScanDataVec.push_back(wifiData);
+  }
 
-    for (const auto& entry : wifiDataMap) {
-        const std::string& mac = entry.first;
-        const std::vector<WiFiScanData>& wifiData = entry.second;
+  std::ofstream outputFile("scan_data.csv");
+  outputFile << "MAC Address,RSSI Value,Mean,STD,Frequency\n";
 
-        if (wifiData.size() == 10) {
-            for (const auto& data : wifiData) {
-                outputFile << data.mac << "," << data.rssi << "," << data.mean << "," << data.std << "," << data.frequency << "\n";
-            }
-        } else if (wifiData.size() > 10) {
-            for (int i = 0; i < 10; ++i) {
-                const auto& data = wifiData[i];
-                outputFile << data.mac << "," << data.rssi << "," << data.mean << "," << data.std << "," << data.frequency << "\n";
-            }
-        }
+  for (const auto &entry : wifiDataMap) {
+    const std::string &mac = entry.first;
+    const std::vector<WiFiScanData> &wifiData = entry.second;
+
+    if (wifiData.size() == 10) {
+      for (const auto &data : wifiData) {
+        outputFile << data.mac << "," << data.rssi << "," << data.mean << ","
+                   << data.std << "," << data.frequency << "\n";
+      }
+    } else if (wifiData.size() > 10) {
+      for (int i = 0; i < 10; ++i) {
+        const auto &data = wifiData[i];
+        outputFile << data.mac << "," << data.rssi << "," << data.mean << ","
+                   << data.std << "," << data.frequency << "\n";
+      }
     }
+  }
 
-    outputFile.close();
+  outputFile.close();
 
-    return true;
+  return true;
 }
